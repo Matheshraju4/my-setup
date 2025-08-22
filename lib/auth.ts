c39dd2901ecafa8206ae9e472439ab4859bf7b54
Matheshraju <101191899+Matheshraju4@users.noreply.github.com>
2025-08-23 00:48:17 +0530

    Update authentication configuration to use PostgreSQL and enable email/password login; add User, Session, Account, and Verification models to Prisma schema.

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,11 +1,13 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
+import prisma from "./prisma";
 // If your Prisma file is located elsewhere, you can change the path
 
-
-const prisma = new PrismaClient();
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
-    provider: "sqlite", // or "mysql", "postgresql", ...etc
+    provider: "postgresql", // or "mysql", "postgresql", ...etc
   }),
+  emailAndPassword: {
+    enabled: true,
+  },
 });
